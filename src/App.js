86eb8db02415b847ea908cce515c3743c0b71b4d
nikolalsvk <nikolaseap@gmail.com>
2017-10-28 13:09:31 +0000

    Install bootstrap and import new logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import { Jumbotron, Grid } from 'react-bootstrap';
 import InsuranceForm from "./InsuranceForm";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to merch-it</h1>
-        </header>
-        <p className="App-intro">Insurances for the wandering travelers</p>
+      <Jumbotron>
+        <Grid>
+          <header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to merch-it</h1>
+          </header>
+          <p className="App-intro">Insurances for the wandering travelers</p>
 
-        <InsuranceForm />
-      </div>
+          <InsuranceForm />
+        </Grid>
+      </Jumbotron>
     );
   }
 }
