0d17529ed1726cd30bd2ac264a2378252a8f3df9
nikolalsvk <nikolaseap@gmail.com>
2017-10-28 12:42:33 +0000

    Initialize InsuranceForm component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InsuranceForm from "./InsuranceForm";
 
 class App extends Component {
   render() {
@@ -11,6 +12,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to merch-it</h1>
         </header>
         <p className="App-intro">Insurances for the wandering travelers</p>
+
+        <InsuranceForm />
       </div>
     );
   }
---
+++ src/InsuranceForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class InsuranceForm extends Component {
+  render() {
+    return ("Hi");
+  }
+}
+
+export default InsuranceForm;
