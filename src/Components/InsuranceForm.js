9eb202efd1470287398256cecd090a8b4b5fd539
nikolalsvk <nikolaseap@gmail.com>
2017-10-28 16:33:15 +0000

    Create form for persons covered in the insurance

--- src/Components/InsuranceForm.js
+++ src/Components/InsuranceForm.js
@@ -1,69 +1,110 @@
 import React, { Component } from 'react';
-import { Form,
+import _ from 'lodash';
+import {
+  Form,
   FormGroup,
   FormControl,
-  ControlLabel,
-  Radio,
   InputGroup,
   Button
 } from 'react-bootstrap';
+
 import FieldGroup from './FieldGroup';
+import PersonForm from './PersonForm';
 
 class InsuranceForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      from: Date.now(),
+      to: Date.now(),
+      region: '',
+      totalNumberOfPersons: 1,
+      numberOfPersons: 0
+    };
+
+    this.handleNumberOfPersonsChange = this.handleNumberOfPersonsChange.bind(
+      this
+    );
+  }
+
+  handleNumberOfPersonsChange(event) {
+    let value = event.target.value;
+    let totalNumberOfPersons = 0;
+
+    if (value < 0 || value > 20) {
+      value = 0;
+    }
+
+    if (value === '') {
+      totalNumberOfPersons = 1;
+    } else {
+      totalNumberOfPersons = value + 1;
+    }
+
+    this.setState({
+      numberOfPersons: value,
+      totalNumberOfPersons: totalNumberOfPersons
+    });
+  }
+
   render() {
     return (
-      <form>
-
+      <div>
         <Form inline>
           <FormGroup>
             <InputGroup>
-                <InputGroup.Button>
-                  <Button>From</Button>
-                </InputGroup.Button>
-              <FormControl type="text" />
+              <InputGroup.Button>
+                <Button>From</Button>
+              </InputGroup.Button>
+              <FormControl
+                type="text"
+                value={this.state.from}
+                onChange={event => this.setState({ from: event.target.value })}
+              />
             </InputGroup>
-          </FormGroup>
-          {' '}
+          </FormGroup>{' '}
           <FormGroup>
             <InputGroup>
-                <InputGroup.Button>
-                  <Button>To</Button>
-                </InputGroup.Button>
-              <FormControl type="text" />
+              <InputGroup.Button>
+                <Button>To</Button>
+              </InputGroup.Button>
+              <FormControl
+                type="text"
+                value={this.state.from}
+                onChange={event => this.setState({ from: event.target.value })}
+              />
             </InputGroup>
           </FormGroup>
         </Form>
         <br />
 
-        <FieldGroup
-          id="region"
-          type="text"
-          label="Region you're visiting"
-          placeholder="Enter region name"
-        />
+        <Form>
+          <FieldGroup
+            id="region"
+            type="text"
+            label="Region you're visiting"
+            placeholder="Enter region name"
+            value={this.state.region}
+            onChange={event => this.setState({ region: event.target.value })}
+          />
 
-        <FieldGroup
-          id="numberOfPersons"
-          type="number"
-          label="Number of persons travelling"
-          placeholder="Enter number of persons travelling with you"
-        />
+          <FieldGroup
+            id="numberOfPersons"
+            type="number"
+            label="Number of persons travelling with you"
+            placeholder="Enter number of persons travelling with you"
+            value={this.state.numberOfPersons}
+            onChange={this.handleNumberOfPersonsChange}
+          />
+        </Form>
 
-        <FormGroup>
-          <ControlLabel>Age range of your group</ControlLabel>{' '}
-          <Radio name="radioGroup" inline>
-            under 18
-          </Radio>
-          {' '}
-          <Radio name="radioGroup" inline checked>
-            18 - 60 years old
-          </Radio>
-          <Radio name="radioGroup" inline>
-            over 60
-          </Radio>
-        </FormGroup>
+        <h3>Information about travellers</h3>
 
-      </form>
+        {_.range(this.state.totalNumberOfPersons).map(key => (
+          <PersonForm key={key} personNumber={key} />
+        ))}
+      </div>
     );
   }
 }
