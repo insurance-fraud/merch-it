0cfeb28c289530acee8a2bb4cc3396e792908fa2
nikolalsvk <nikolaseap@gmail.com>
2017-10-28 12:32:03 +0000

    Edit title and description

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to merch-it</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Insurances for the wandering travelers</p>
       </div>
     );
   }
